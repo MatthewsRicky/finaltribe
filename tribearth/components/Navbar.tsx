c9854637994bc68c41ffcc66bd89d7c98a6e9d1a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-17 21:58:27 +0300

    Update Navbar.tsx

--- tribearth/components/Navbar.tsx
+++ tribearth/components/Navbar.tsx
@@ -1,8 +1,19 @@
 "use client"
 
+import Image from "next/image"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex justify-between items-center gap-4 px-4 py-6">
+      <div>
+        <Image 
+          src={"/images/Tribearth logo.png"}
+          alt="logo"
+          width={80}
+          height={80}
+        />
+      </div>
+    </div>
   )
 }
 
