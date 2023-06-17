6dbbd8ad226e091a2989140c8316bff0d7f5a156
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-17 23:17:42 +0300

    Update page.tsx

--- tribearth/app/page.tsx
+++ tribearth/app/page.tsx
@@ -1,11 +1,13 @@
 import Artisans from '@/components/Artisans'
+import Ethos from '@/components/Ethos'
 import Hero from '@/components/Hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex justify-center items bg-neutral-100 text-black relative w-full h-full'>
+    <div className='mt-20 flex flex-col justify-center items bg-neutral-100 text-black relative w-full h-full'>
       <Hero />
+      <Ethos />
       <Artisans />
     </div>
   )
