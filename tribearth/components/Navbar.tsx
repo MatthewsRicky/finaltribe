3b906becc2596831418648512e42dcd8069aa69e
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-17 23:23:06 +0300

    Update Navbar.tsx

--- tribearth/components/Navbar.tsx
+++ tribearth/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 const Navbar = () => {
   return (
-    <div className="absolute w-full top-0 right-0 left-0 bg-emerald-100/90 flex justify-between items-center gap-4 px-4 py-6">
+    <div className="hidden md:flex absolute w-full top-0 right-0 left-0 bg-emerald-100/90 justify-between items-center gap-4 px-4 py-6">
       <div>
         <Image 
           src={"/images/Tribearth logo.png"}
