7dd4543f61f472f98b04bebfec704372aede36b0
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-17 23:20:45 +0300

    Update page.tsx

--- tribearth/app/page.tsx
+++ tribearth/app/page.tsx
@@ -1,6 +1,9 @@
 import Artisans from '@/components/Artisans'
+import Carousel from '@/components/Carousel'
 import Ethos from '@/components/Ethos'
 import Hero from '@/components/Hero'
+import Learn from '@/components/Learn'
+import Location from '@/components/Location'
 import Image from 'next/image'
 
 export default function Home() {
@@ -8,7 +11,10 @@ export default function Home() {
     <div className='mt-20 flex flex-col justify-center items-center bg-neutral-100 text-black relative w-full h-full'>
       <Hero />
       <Ethos />
+      <Location />
       <Artisans />
+      <Carousel />
+      <Learn />
     </div>
   )
 }
