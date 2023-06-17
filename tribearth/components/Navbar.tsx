05dedeafa0166c2f2edcf417c3d63c19677448ed
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-17 23:24:22 +0300

    Update Navbar.tsx

--- tribearth/components/Navbar.tsx
+++ tribearth/components/Navbar.tsx
@@ -1,8 +1,14 @@
 "use client"
 
 import Image from "next/image"
+import { useState } from "react"
 
 const Navbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  
+
   return (
     <div className="hidden md:flex absolute w-full top-0 right-0 left-0 bg-emerald-100/90 justify-between items-center gap-4 px-4 py-6">
       <div>
