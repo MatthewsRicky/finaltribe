83caed391a5f10f0e63edd9a11c519fde8ef7cf8
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-17 23:21:12 +0300

    Update layout.tsx

--- tribearth/app/layout.tsx
+++ tribearth/app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <Navbar />
       <body className={inter.className}>{children}</body>
+      <Footer />
     </html>
   )
 }
