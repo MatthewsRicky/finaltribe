9e22d659e1ca37fb35b1719878b7e6ef4ad36c38
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-21 23:40:39 +0300

    Update Hero.tsx

--- tribearth/components/Hero.tsx
+++ tribearth/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="hero flex justify-center items-center relative">
       <button 
         onClick={() => {}}
-        className="bg py-4 px-3 text-center text-xl md:text-2xl lg:text-3xl font-semibold lg:font-bold hover:scale-105 cursor-pointer transition-all duration-300"
+        className="bg-emerald-200 rounded-lg py-4 px-3 text-center text-xl md:text-2xl lg:text-3xl font-semibold lg:font-bold hover:scale-105 cursor-pointer transition-all duration-300"
       >
         Explore
       </button>
