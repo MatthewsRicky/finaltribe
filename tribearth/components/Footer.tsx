15e4f20d2e8897df6deb52aad545046b7a5f445a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-21 21:47:31 +0300

    Update Footer.tsx

--- tribearth/components/Footer.tsx
+++ tribearth/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <div className="w-full relative bg-emerald-100/90 justify-center items-center flex flex-col">
+    <div className="w-full bg-emerald-100/90 justify-around items-center flex flex-col">
       <div className="flex flex-col md:flex-row justify-center md:justify-between items-center px-10 gap-4">
         <div>
           <h1>CUSTOMER DETAILS</h1>
