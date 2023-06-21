a7da0984a6686b8b3b09b10a5c7d56eb6f170d54
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-21 21:10:06 +0300

    Update Footer.tsx

--- tribearth/components/Footer.tsx
+++ tribearth/components/Footer.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <div className="w-full relative bg-emerald-100/90 justify-center items-center flex flex-col">
-      <div className="flex justify-center items-center gap-4">
+      <div className="flex justify-between items-center gap-4">
         <div>CONTACT DETAILS</div>
         <div>
           <div>
@@ -17,8 +17,8 @@ const Footer = () => {
         <div>CONTACT US</div>
         <div>SUBSCRIBE</div>
       </div>
-      <div>Copyright 2023. Tribearth.</div>
-      <div>All Right Reserved</div>
+      <div className="w-full text-center bg-emerald-200/90">Copyright 2023. Tribearth.</div>
+      <div className="w-full text-center bg-emerald-300/90">All Right Reserved</div>
     </div>
   )
 }
