f7e8650107766604569c681da541b4bd4eae30e1
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-22 21:40:47 +0300

    Update page.tsx

--- tribearth/app/about/page.tsx
+++ tribearth/app/about/page.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 const About = () => {
   return (
     <div>
-      <div className="relative bg-yellow-500/60 rounded-md shadow-xl text-slate-500 m-4 flex flex-col md:flex-row justify-between gap-4 p-4">
+      <div className="relative bg-yellow-500/60 rounded-md shadow-lg text-slate-500 m-4 flex flex-col md:flex-row justify-between gap-4 p-4">
         <div className="flex flex-col gap-4 w-80">
           <h1>ABOUT US</h1>
           <p>Tribearth is a socially conscious jewelry business that creates a community of empowered women & youth who produce handmade products in a sustainable and ethical environment. Using a moralistic and economic approach, Tribearth creates a challenging voice against the detrimental impact of fast fashion and inequality faced by stigmatized women & youth.</p>
         </div>
-        <div className="w-full">
+        <div className="w-full rounded-lg p-0 shadow-md">
           <Image 
             src={"/images/abouthero.png"}
             alt="About Hero Image"
@@ -18,7 +18,17 @@ const About = () => {
           />
         </div>
       </div>
-      <div></div>
+      <div>
+        <div>
+          <Image 
+            src={"/images/green.png"}
+            alt="About Hero Image"
+            width={1000}
+            height={1000}    
+          />
+        </div>
+        <div></div>
+      </div>
       <div></div>
       <div></div>
     </div>
