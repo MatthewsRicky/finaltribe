5bac3a427c10f95e99deaab99954b24c960e49e2
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-22 13:20:40 +0300

    Update DropDownMenu.tsx

--- tribearth/components/DropDownMenu.tsx
+++ tribearth/components/DropDownMenu.tsx
@@ -14,25 +14,25 @@ const DropdownMenu = () => {
     <div className="relative">
       <button
         onClick={toggleMenu}
-        className="flex text-center items-center text-yellow-500 hover:bg-yellow-500 py-4 px-2 hover:text-white rounded-md"
+        className="flex text-center items-center text-yellow-500 hover:bg-yellow-500 py-4 px-2 hover:text-white rounded-md "
       >
         Shop
       </button>
 
       {isOpen && (
-        <div className="absolute rounded-lg right-0 m-2 w-48 bg-emerald-400 flex flex-col gap-4">
+        <div className="absolute rounded-lg right-0 m-2 w-48 bg-emerald-400 flex flex-col gap-4 hover:rounded-md">
           <Link href="/shop/bracelets"
-            className="px-4 py-2 hover:bg-yellow-500/95">Bracelets
+            className="px-4 py-2 hover:bg-yellow-500/95 hover:rounded-md">Bracelets
           </Link>
           <Link href="/shop/earrings"
-            className="px-4 py-2 hover:bg-yellow-500/95">
+            className="px-4 py-2 hover:bg-yellow-500/95 hover:rounded-md">
               Earrings
             </Link>
           <Link href="/shop/necklaces"
-            className="px-4 py-2 hover:bg-yellow-500/95">Necklaces 
+            className="px-4 py-2 hover:bg-yellow-500/95 hover:rounded-md">Necklaces 
           </Link>
           <Link href="/shop/rings"
-            className="px-4 py-2 hover:bg-yellow-500/95">Rings 
+            className="px-4 py-2 hover:bg-yellow-500/95 hover:rounded-md">Rings 
           </Link>
         </div>
       )}
