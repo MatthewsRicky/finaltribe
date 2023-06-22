09fe38049f21145d2c2f3b630428727b19d44790
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-22 18:23:59 +0300

    Update page.tsx

--- tribearth/app/about/page.tsx
+++ tribearth/app/about/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const About = () => {
   return (
     <div>
-      <div className="relative justify-between gap-8">
-        <div className="flex flex-col w-80">
+      <div className="relative flex flex-col md:flex-row justify-between gap-8 p-8">
+        <div className="flex flex-col gap-8 w-80">
           <h1>ABOUT US</h1>
           <p>Tribearth is a socially conscious jewelry business that creates a community of empowered women & youth who produce handmade products in a sustainable and ethical environment. Using a moralistic and economic approach, Tribearth creates a challenging voice against the detrimental impact of fast fashion and inequality faced by stigmatized women & youth.</p>
         </div>
