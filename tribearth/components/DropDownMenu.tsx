f367385b41007778d6a6190a7ff400d3af9a1013
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-22 12:42:23 +0300

    Update DropDownMenu.tsx

--- tribearth/components/DropDownMenu.tsx
+++ tribearth/components/DropDownMenu.tsx
@@ -14,13 +14,13 @@ const DropdownMenu = () => {
     <div className="relative">
       <button
         onClick={toggleMenu}
-        className="flex items-center text-emerald-300"
+        className="flex text-center items-center text-yellow-500"
       >
         Shop
       </button>
 
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-48 bg-emerald-300 flex flex-col gap-4">
+        <div className="absolute rounded-lg right-0 m-2 w-48 bg-yellow-400 flex flex-col gap-4">
           <Link href="/shop/bracelets"
             className="px-4 py-2 hover:bg-emerald-400">Bracelets
           </Link>
