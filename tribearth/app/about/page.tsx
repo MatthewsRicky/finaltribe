b18dfd778406a60d220eb0ad1c65c936c09be3f2
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-22 21:56:16 +0300

    Update page.tsx

--- tribearth/app/about/page.tsx
+++ tribearth/app/about/page.tsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 const About = () => {
   return (
     <div>
-      <div className="relative bg-yellow-500/60 rounded-md shadow-lg text-slate-500 m-4 flex flex-col md:flex-row justify-between gap-4 p-4">
+      <div className="relative bg-emerald-200/10 rounded-md shadow-lg 
+      text-slate-500 m-4 flex flex-col md:flex-row justify-between gap-4 p-4">
         <div className="flex flex-col gap-4 w-80">
           <h1>ABOUT US</h1>
           <p>Tribearth is a socially conscious jewelry business that creates a community of empowered women & youth who produce handmade products in a sustainable and ethical environment. Using a moralistic and economic approach, Tribearth creates a challenging voice against the detrimental impact of fast fashion and inequality faced by stigmatized women & youth.</p>
@@ -18,8 +19,9 @@ const About = () => {
           />
         </div>
       </div>
-      <div>
-        <div>
+      <div className="relative bg-emerald-200/10  rounded-md shadow-lg 
+      text-slate-500 m-4 flex flex-col md:flex-row justify-between gap-4 p-4">
+        <div className="w-full rounded-lg p-0 shadow-md">
           <Image 
             src={"/images/green.png"}
             alt="About Hero Image"
@@ -27,12 +29,15 @@ const About = () => {
             height={1000}    
           />
         </div>
-        <div>
+        <div className="flex justify-around flex-col">
           <h1>VALUES & PHILOSOPHY</h1>
+          <div>
           <p>Sustainability</p>
           <p>Ethical trade</p>
           <p>Creative innovation</p>
           <p>Passion.</p>
+          </div>
+          
         </div>
       </div>
       <div></div>
