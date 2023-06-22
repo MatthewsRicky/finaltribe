089ec61ec1ce3474eeaa1bd9018c01130d1ad106
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-22 15:35:19 +0300

    Update page.tsx

--- tribearth/app/about/page.tsx
+++ tribearth/app/about/page.tsx
@@ -2,7 +2,18 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div>
+        <div>
+          <h1>ABOUT US</h1>
+          <p>Tribearth is a socially conscious jewelry business that creates a community of empowered women & youth who produce handmade products in a sustainable and ethical environment. Using a moralistic and economic approach, Tribearth creates a challenging voice against the detrimental impact of fast fashion and inequality faced by stigmatized women & youth.</p>
+        </div>
+        <div></div>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   )
 }
 
