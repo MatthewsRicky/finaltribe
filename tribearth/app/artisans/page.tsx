89df8c23aa77b1973c15392d5651c1f505481f56
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-23 15:03:48 +0300

    Update page.tsx

--- tribearth/app/artisans/page.tsx
+++ tribearth/app/artisans/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image"
 
 const Artisans = () => {
   return (
-    <div>Artisans</div>
+    <div>
+      <h1></h1>
+      <div>
+        <Image />
+        <div>
+          <h1>FLORENCE IMIZA - Assembling Chief</h1>
+          <p>My name is Florence. I have four children. I’ve always believed that life is about supporting each other because if we take care of one another, everyone grows. I want to see my daughter grow up to be empowered just like me. Here at Tribearth, I have found peace and happiness in the work I do and I see a hopeful future ahead.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
