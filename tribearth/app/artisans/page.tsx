02d4b9fb7d2596e7a6d642bbde050f04eccafb68
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-23 15:05:18 +0300

    Update page.tsx

--- tribearth/app/artisans/page.tsx
+++ tribearth/app/artisans/page.tsx
@@ -5,7 +5,12 @@ const Artisans = () => {
     <div>
       <h1></h1>
       <div>
-        <Image />
+        <Image
+          src={"/images/Florence.jpg"}
+          alt="Florence"
+          width={1000}
+          height={1000}
+        />
         <div>
           <h1>FLORENCE IMIZA - Assembling Chief</h1>
           <p>My name is Florence. I have four children. I’ve always believed that life is about supporting each other because if we take care of one another, everyone grows. I want to see my daughter grow up to be empowered just like me. Here at Tribearth, I have found peace and happiness in the work I do and I see a hopeful future ahead.</p>
