808d5b51441db383ec6672d428a86da01ade1fb2
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-23 17:39:31 +0300

    Update page.tsx

--- tribearth/app/artisans/page.tsx
+++ tribearth/app/artisans/page.tsx
@@ -4,7 +4,7 @@ const Artisans = () => {
   return (
     <div className="relative w-full flex flex-col">
       <h1>Each artisan we employ has a story and we want you to know it</h1>
-      <div className="flex gap-4 py-2">
+      <div className="flex gap-4 py-2 px-4 justify-center items-center">
         <Image
           src={"/images/Florence.jpg"}
           alt="Florence"
@@ -16,7 +16,7 @@ const Artisans = () => {
           <p>My name is Florence. I have four children. I’ve always believed that life is about supporting each other because if we take care of one another, everyone grows. I want to see my daughter grow up to be empowered just like me. Here at Tribearth, I have found peace and happiness in the work I do and I see a hopeful future ahead.</p>
         </div>
       </div>
-      <div className="flex gap-4 py-2">
+      <div className="flex gap-4 py-2 px-4 justify-center items-center">
          <div>
           <h1>FRED NYONGESA - Soldering King</h1>
           <p>Hi, I’m Fred. I’ve been working for Tribearth for 2 years now and it’s helped me sustain myself and my family. I do all the soldering work at the workshop but my passion is creating new designs that suit Tribearth’s style. If you have a style in mind, just reach out to us and I’ll custom make a special piece just for you. </p>
@@ -28,7 +28,7 @@ const Artisans = () => {
           height={1000}
         />
       </div>
-      <div className="flex gap-4 py-2">
+      <div className="flex gap-4 py-2 px-4 justify-center items-center">
         <Image
           src={"/images/Florence.jpg"}
           alt="Florence"
@@ -40,7 +40,7 @@ const Artisans = () => {
           <p>My name is Florence. I have four children. I’ve always believed that life is about supporting each other because if we take care of one another, everyone grows. I want to see my daughter grow up to be empowered just like me. Here at Tribearth, I have found peace and happiness in the work I do and I see a hopeful future ahead.</p>
         </div>
       </div>
-      <div className="flex gap-4 py-2">
+      <div className="flex gap-4 py-2 px-4 justify-center items-center">
          <div>
           <h1>FRED NYONGESA - Soldering King</h1>
           <p>Hi, I’m Fred. I’ve been working for Tribearth for 2 years now and it’s helped me sustain myself and my family. I do all the soldering work at the workshop but my passion is creating new designs that suit Tribearth’s style. If you have a style in mind, just reach out to us and I’ll custom make a special piece just for you. </p>
