f4291e84a26aa936e3afd8f04115f123cc27ad16
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-24 18:53:39 +0300

    Update page.tsx

--- tribearth/app/about/page.tsx
+++ tribearth/app/about/page.tsx
@@ -6,11 +6,11 @@ const About = () => {
     <div>
       <div className="relative wrap bg-emerald-200/10 rounded-md shadow-lg 
       text-slate-500 m-4 flex flex-col md:flex-row justify-between gap-4 p-4">
-        <div className="flex min-w-[250px] justify-center items-center text-center flex-col gap-4 w-80">
+        <div className="flex min-w-[50%] justify-center items-center text-center flex-col gap-4 w-80">
           <h1>ABOUT US</h1>
           <p>Tribearth is a socially conscious jewelry business that creates a community of empowered women & youth who produce handmade products in a sustainable and ethical environment. Using a moralistic and economic approach, Tribearth creates a challenging voice against the detrimental impact of fast fashion and inequality faced by stigmatized women & youth.</p>
         </div>
-        <div className="w-full flex rounded-lg p-0 shadow-md">
+        <div className="w-full rounded-lg p-0 shadow-md">
           <Image 
             src={"/images/abouthero.png"}
             alt="About Hero Image"
@@ -29,7 +29,7 @@ const About = () => {
             height={1000}    
           />
         </div>
-        <div className="flex justify-around flex-col">
+        <div className="flex min-w-[50%] justify-center items-center text-center flex-col gap-4 w-80">
           <h1>VALUES & PHILOSOPHY</h1>
           <div>
             <p>Sustainability</p>
@@ -42,7 +42,7 @@ const About = () => {
       <div>
          <div className="relative bg-emerald-200/10 rounded-md shadow-lg 
       text-slate-500 m-4 flex flex-col md:flex-row justify-between gap-4 p-4">
-        <div className="flex flex-col gap-4 w-80">
+        <div className="flex min-w-[50%] justify-center items-center text-center flex-col gap-4 w-80">
           <h1>TEAM LOCALAID</h1>
             <p>
               We are a Social Enterprise under our affiliated NGO called LocalAid. LocalAid has 10 years of experience in the NGO sector in terms of measuring impact, social interaction, and outreach programs. LocalAid's vision is to empower marginalized communities and vulnerable children. Tribearth benefits from LocaLAid's organizational architecture, such as legal framework, HR and accounting.
@@ -69,7 +69,7 @@ const About = () => {
             height={1000}    
           />
         </div>
-        <div className="flex justify-around flex-col">
+        <div className="flex min-w-[50%] justify-center items-center text-center flex-col gap-4 w-80">
           <h1>DESIGNER</h1>
             <p>Sumer Homeh is our head creative designer. She had studied and worked as a fashion designer for 7 years in Australia before founding LocalAid in 2008. She has lead several projects through her humanitarian work in Kenya over the past 10 years with LocalAid. </p>
         </div>
