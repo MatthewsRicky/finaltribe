45ca53b6c9241c0158acc53665ab1c71891569fa
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-24 18:05:28 +0300

    Update page.tsx

--- tribearth/app/about/page.tsx
+++ tribearth/app/about/page.tsx
@@ -4,17 +4,17 @@ import Image from "next/image";
 const About = () => {
   return (
     <div>
-      <div className="relative bg-emerald-200/10 rounded-md shadow-lg 
+      <div className="relative wrap bg-emerald-200/10 rounded-md shadow-lg 
       text-slate-500 m-4 flex flex-col md:flex-row justify-between gap-4 p-4">
-        <div className="flex flex-col gap-4 w-80">
+        <div className="flex min-w-[250px] justify-center items-center text-center flex-col gap-4 w-80">
           <h1>ABOUT US</h1>
           <p>Tribearth is a socially conscious jewelry business that creates a community of empowered women & youth who produce handmade products in a sustainable and ethical environment. Using a moralistic and economic approach, Tribearth creates a challenging voice against the detrimental impact of fast fashion and inequality faced by stigmatized women & youth.</p>
         </div>
-        <div className="w-full rounded-lg p-0 shadow-md">
+        <div className="w-full flex rounded-lg p-0 shadow-md">
           <Image 
             src={"/images/abouthero.png"}
             alt="About Hero Image"
-            width={1000}
+            width={500}
             height={1000}    
           />
         </div>
