094668f1667ec3be7b6530ccfc6dad0e89845286
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-25 22:20:29 +0300

    Update page.tsx

--- tribearth/app/impact/page.tsx
+++ tribearth/app/impact/page.tsx
@@ -24,15 +24,17 @@ const Impact = () => {
            Tribearth sustains 30% of LocalAid’s humanitarian work. To learn more of LocalAid’s work in Kenya, please visit <Link href ="/">localaid.or.ke</Link>
         </h1>
       </div>
-      <div>
-        <Image src={"/images/Fairtrade.jpg"} alt="Fair Trade" width={700} height={1000} />
+      <div className="grid gap-x-6 gap-y-6 grid-row-1 md:grid-cols-2 justify-around items-center relative transition-all hover:bg-gradient-to-b from-yellow-500/20 to-emerald-300/20 duration-300 p-8 my-4 mx-4 shadow-lg">
+        <Image
+          className="min-w-[50%] rounded-lg hover:scale-110"
+          src={"/images/Fairtrade.jpg"} alt="Fair Trade" width={700} height={1000} />
         <div>
           <h1>Fair Trade</h1>
           <h2>FASHION IS THE LARGEST INDUSTRIAL EMPLOYER OF WOMEN AND LESS THAN 2% ARE ACTUALLY PAID A LIVING WAGE.</h2>
           <p>Our vision is to empower women by providing them with fair and ethical means in which to earn a living. We promote gender equality in a sector that have long been male dominated. We place special emphasis on training and development that empowers the women working at the workshop.</p>
         </div>
       </div>
-      <div>
+      <div className="grid gap-x-6 gap-y-6 grid-row-1 md:grid-cols-2 justify-around items-center relative transition-all hover:bg-gradient-to-b from-yellow-500/20 to-emerald-300/20 duration-300 p-8 my-4 mx-4 shadow-lg">
          <div>
           <h1>Green Workshop</h1>
           <p>We are passionate about preserving our planet for future generations. Our workshop, based in Diani, Kenya, operates using sustainable interventions, such as our recycling plant which includes recycling waste material and water. Additionally, we recycle our waste metals to re-create designs. We are working on setting up solar systems for our power and water harvesting systems.</p>
@@ -44,7 +46,7 @@ const Impact = () => {
           height={1000}
         />
       </div>
-      <div>
+      <div className="grid gap-x-6 gap-y-6 grid-row-1 md:grid-cols-2 justify-around items-center relative transition-all hover:bg-gradient-to-b from-yellow-500/20 to-emerald-300/20 duration-300 p-8 my-4 mx-4 shadow-lg">
          <div>
           <Image
             src={"/images/Womenempowerment.jpeg"}
