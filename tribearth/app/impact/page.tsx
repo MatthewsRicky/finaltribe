5a3aa8d696fd91f4dce8b93e60db8c89c94757eb
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-25 16:33:05 +0300

    Update page.tsx

--- tribearth/app/impact/page.tsx
+++ tribearth/app/impact/page.tsx
@@ -1,11 +1,27 @@
 "use client";
 
+import Image from "next/image";
+
 const Impact = () => {
   return (
     <div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        <div>
+          <h1></h1>
+          <p></p>
+        </div>
+        <Image />
+      </div>
+      <div>
+        <h1></h1>
+      </div>
+      <div>
+        <Image />
+        <div>
+          <h1></h1>
+          <p></p>
+        </div>
+      </div>
       <div></div>
       <div></div>
     </div>
