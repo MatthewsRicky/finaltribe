411b69ce6dcd08efe45de4f857c68280a262522b
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-25 22:42:35 +0300

    Update page.tsx

--- tribearth/app/impact/page.tsx
+++ tribearth/app/impact/page.tsx
@@ -8,7 +8,7 @@ const Impact = () => {
     <div className="w-full relative flex flex-col">
       <div className="grid gap-x-6 gap-y-6 grid-row-1 md:grid-cols-2 justify-around items-center relative transition-all hover:bg-gradient-to-b from-yellow-500/20 to-emerald-300/20 duration-300 p-8 my-4 mx-4 shadow-lg">
         <div className="flex flex-col gap-y-2 shadow-lg hover:scale-105 hover:bg-white">
-          <h1 className="flex font-semibold justify-center m-2 p-2 rounded-md z-10 bg-yellow-500/40 left-[40%] text-xl md:text-2xl shadow-lg hover:scale-105">Ethical Gemstones</h1>
+          <h1  className="flex font-semibold justify-center m-2 p-2 rounded-md z-10 bg-yellow-500/40 left-[40%] text-xl md:text-2xl shadow-lg hover:scale-105">Ethical Gemstones</h1>
           <p className="flex p-8 text-lg md:text-xl mt-4">Responsible sourcing of gemstones is of great importance to our brand. Our gemstones are sourced directly from ethical mines run by women in Kenya. We embrace traceability and sustainability throughout our complete supply chain.</p>
         </div>
         <Image
@@ -29,7 +29,7 @@ const Impact = () => {
           className="min-w-[50%] rounded-lg hover:scale-110"
           src={"/images/Fairtrade.jpg"} alt="Fair Trade" width={700} height={1000} />
         <div>
-          <h1>Fair Trade</h1>
+          <h1 className="flex font-semibold justify-center m-2 p-2 rounded-md z-10 bg-yellow-500/40 left-[40%] text-xl md:text-2xl shadow-lg hover:scale-105">Fair Trade</h1>
           <h2>FASHION IS THE LARGEST INDUSTRIAL EMPLOYER OF WOMEN AND LESS THAN 2% ARE ACTUALLY PAID A LIVING WAGE.</h2>
           <p>Our vision is to empower women by providing them with fair and ethical means in which to earn a living. We promote gender equality in a sector that have long been male dominated. We place special emphasis on training and development that empowers the women working at the workshop.</p>
         </div>
