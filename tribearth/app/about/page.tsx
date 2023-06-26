084d7bc1dc1f5e53ffdaf26b8b4270e4ae4383b9
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-26 10:58:17 +0300

    Update page.tsx

--- tribearth/app/about/page.tsx
+++ tribearth/app/about/page.tsx
@@ -8,7 +8,7 @@ const About = () => {
       text-slate-500 m-4 flex flex-col md:flex-row justify-between gap-4 p-4">
         <div className="flex min-w-[50%] justify-center items-center text-center flex-col gap-4 w-80">
           <h1 className="font-semibold text-xl shadow-md px-4 rounded-lg shadow-opacity-60 bg-yellow-500/20">ABOUT US</h1>
-          <p>Tribearth is a socially conscious jewelry business that creates a community of empowered women & youth who produce handmade products in a sustainable and ethical environment. Using a moralistic and economic approach, Tribearth creates a challenging voice against the detrimental impact of fast fashion and inequality faced by stigmatized women & youth.</p>
+          <p className="leading-7">Tribearth is a socially conscious jewelry business that creates a community of empowered women & youth who produce handmade products in a sustainable and ethical environment. Using a moralistic and economic approach, Tribearth creates a challenging voice against the detrimental impact of fast fashion and inequality faced by stigmatized women & youth.</p>
         </div>
         <div className="w-full rounded-lg p-0 shadow-md">
           <Image 
@@ -44,7 +44,7 @@ const About = () => {
       text-slate-500 m-4 flex flex-col md:flex-row justify-between gap-4 p-4">
         <div className="flex min-w-[50%] justify-center items-center text-center flex-col gap-4 w-80">
           <h1 className="font-semibold text-xl shadow-md px-4 rounded-lg shadow-opacity-60 bg-yellow-500/20">TEAM LOCALAID</h1>
-            <p>
+            <p className="leading-7">
               We are a Social Enterprise under our affiliated NGO called LocalAid. LocalAid has 10 years of experience in the NGO sector in terms of measuring impact, social interaction, and outreach programs. LocalAid's vision is to empower marginalized communities and vulnerable children. Tribearth benefits from LocaLAid's organizational architecture, such as legal framework, HR and accounting.
             </p>
         </div>
@@ -71,7 +71,7 @@ const About = () => {
         </div>
         <div className="flex min-w-[50%] justify-center items-center text-center flex-col gap-4 w-80">
           <h1 className="font-semibold text-xl shadow-md px-4 rounded-lg shadow-opacity-60 bg-yellow-500/20">DESIGNER</h1>
-            <p>Sumer Homeh is our head creative designer. She had studied and worked as a fashion designer for 7 years in Australia before founding LocalAid in 2008. She has lead several projects through her humanitarian work in Kenya over the past 10 years with LocalAid. </p>
+            <p className="leading-7">Sumer Homeh is our head creative designer. She had studied and worked as a fashion designer for 7 years in Australia before founding LocalAid in 2008. She has lead several projects through her humanitarian work in Kenya over the past 10 years with LocalAid. </p>
         </div>
       </div>
       </div>
