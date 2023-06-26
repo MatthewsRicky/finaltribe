7b89713644e8ae43cd5cc82de06d771c405b7cdb
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-26 12:11:02 +0300

    Update page.tsx

--- tribearth/app/vegancafe/page.tsx
+++ tribearth/app/vegancafe/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import Link from "next/link";
+
 
 const Vegancafe = () => {
   return (
-    <div>Vegancafe</div>
+    <div>
+      <div>
+        <h1>Vegan Boutique Cafe</h1>
+        
+          <Link>
+            <p></p>
+          </Link>
+        
+      </div>
+    </div>
   )
 }
 
