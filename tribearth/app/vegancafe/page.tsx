b1517c6cea0612a750126fd5a0d4e4a438fb024e
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-10 23:16:19 +0300

    Update page.tsx

--- tribearth/app/vegancafe/page.tsx
+++ tribearth/app/vegancafe/page.tsx
@@ -83,7 +83,7 @@ const Vegancafe = () => {
 					</div>
 				</div>
 			</div>
-			<div className="w-[50%]">
+			<div className="w-[50%] m-4 flex">
 				<Card
 					title={"Ethiopian Platter"}
 					image={"/images/anjeera.jpeg"}
