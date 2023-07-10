b08632bb088072c6755b2bfdaa519d4cbd5bb0c0
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-10 23:24:01 +0300

    Update Hero.tsx

--- tribearth/components/Hero.tsx
+++ tribearth/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
 	const [isClicked, setIsClicked] = useState(false);
 
 	return (
-		<div className="hero flex justify-center items-center relative">
+		<div className="hero rounded-full justify-center flex items-center relative">
 			<button
 				onClick={() => {}}
 				className="bg-yellow-400/80 rounded-lg py-4 px-3 text-center hover:shadow-emerald-200/70 hover:shadow-lg text-slate-700/80 text-xl md:text-2xl lg:text-3xl font-semibold lg:font-bold hover:scale-105 cursor-pointer transition-all duration-300"
