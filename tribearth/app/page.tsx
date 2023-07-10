7724d75d1d08a163c78efd6e867a9898c792f05c
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-10 23:51:36 +0300

    Update page.tsx

--- tribearth/app/page.tsx
+++ tribearth/app/page.tsx
@@ -1,24 +1,22 @@
-import Artisans from '@/components/Artisans'
-import Carousel from '@/components/Carousel'
-import Ethos from '@/components/Ethos'
-import Hero from '@/components/Hero'
-import Learn from '@/components/Learn'
-import Location from '@/components/Location'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Artisans from "@/components/Artisans";
+import Carousel from "@/components/Carousel";
+import Ethos from "@/components/Ethos";
+import Hero from "@/components/Hero";
+import Learn from "@/components/Learn";
+import Location from "@/components/Location";
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className='mt-20 flex flex-col justify-center items-center bg-neutral-100 text-black relative w-full h-full'>
-      <Hero />
-      <Ethos />
-      <Location />
-      <Artisans />
-      <Carousel />
-      <Learn />
-    </div>
-  )
+	return (
+		<div className="mt-20 flex flex-col justify-center items-center bg-neutral-100 text-black relative w-full h-full">
+			<Hero />
+			<Location />
+			<Artisans />
+			<Carousel />
+			<Learn />
+		</div>
+	);
 }
